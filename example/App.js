f370c945332b53f70cca3609872d5dc9940f1555
hoangnm <nguyenminhhoang210891@gmail.com>
2017-12-20 02:46:01 +0700

    events

--- example/App.js
+++ example/App.js
@@ -12,10 +12,43 @@ import {
 import WeekView from 'react-native-week-view';
 
 export default class App extends Component<{}> {
+  generateDates = (hours, minutes) => {
+    const date = new Date();
+    date.setHours(date.getHours() + hours);
+    if (minutes != null) {
+      date.setMinutes(0);
+    }
+    return date;
+  };
+
   render() {
+    const events = [
+      {
+        id: 1,
+        description: 'Truck 1',
+        startDate: new Date(),
+        endDate: this.generateDates(3),
+        color: 'blue',
+      },
+      {
+        id: 2,
+        description: 'Truck 2',
+        startDate: new Date(),
+        endDate: this.generateDates(2),
+        color: 'red',
+      },
+      {
+        id: 3,
+        description: 'Truck 3',
+        startDate: this.generateDates(5),
+        endDate: this.generateDates(8),
+        color: 'green',
+      },
+    ];
+
     return (
       <View style={styles.container}>
-        <WeekView />
+        <WeekView events={events} />
       </View>
     );
   }
