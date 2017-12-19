2ad1f55a5ba3a5fa3f3e8b3447eccd1999678896
hoangnm <nguyenminhhoang210891@gmail.com>
2017-12-20 02:01:44 +0700

    view for one day

--- src/WeekView.js
+++ src/WeekView.js
@@ -1,11 +1,61 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import {
+  Text,
+  View,
+  ScrollView,
+} from 'react-native';
+
+import styles from './styles';
+
+const TIME_LABELS_COUNT = 48;
+
+const TimeRow = () => {
+  return (
+    <View style={styles.timeRow}>
+      <View style={styles.timeLabelLine} />
+    </View>
+  );
+};
+
+const TimeLabel = ({ time }) => { // eslint-disable-line react/prop-types
+  return (
+    <View style={styles.timeLabel}>
+      <Text style={styles.timeText}>{time}</Text>
+    </View>
+  );
+};
 
 class WeekView extends Component {
+  generateTimes = () => {
+    const times = [];
+    for (let i = 0; i < TIME_LABELS_COUNT; i += 1) {
+      const minutes = i % 2 === 0 ? '00' : '30';
+      const hour = Math.floor(i / 2);
+      const time = `${hour}:${minutes}`;
+      times.push(time);
+    }
+    return times;
+  };
+
   render() {
+    const times = this.generateTimes();
+
     return (
-      <View>
-        <Text>WeekView</Text>
+      <View style={styles.container}>
+        <ScrollView>
+          <View style={styles.timeLineContainer}>
+            <View style={styles.timeColumn}>
+              {times.map((time) => {
+                return (<TimeLabel key={`${time}`} time={time} />);
+              })}
+            </View>
+            <View style={styles.eventColumn}>
+              {times.map((time) => {
+                return (<TimeRow key={time} time={time} />);
+              })}
+            </View>
+          </View>
+        </ScrollView>
       </View>
     );
   }
---
+++ src/styles.js
@@ -0,0 +1,46 @@
+import { StyleSheet } from 'react-native';
+
+const GREY_COLOR = '#E9EDF0';
+const LIGHT_COLOR = '#FFF';
+const ROW_HEIGHT = 40;
+const CONTENT_OFFSET = 16;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  timeLineContainer: {
+    flexDirection: 'row',
+  },
+  timeRow: {
+    flex: 0,
+    height: ROW_HEIGHT,
+  },
+  timeLabelLine: {
+    height: 1,
+    backgroundColor: GREY_COLOR,
+    position: 'absolute',
+    right: 0,
+    left: 10,
+  },
+  timeColumn: {
+    flex: -1,
+    paddingTop: 10,
+    width: 40,
+  },
+  timeLabel: {
+    flex: -1,
+    height: 40,
+  },
+  timeText: {
+    fontSize: 12,
+    textAlign: 'center',
+  },
+  eventColumn: {
+    flex: 1,
+    paddingTop: CONTENT_OFFSET,
+    backgroundColor: LIGHT_COLOR,
+  },
+});
+
+export default styles;
