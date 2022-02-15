8dd4417f2a89b94abed862c0223e79b73da0bdaa
Hong sa sung <scvhss@naver.com>
2022-02-15 21:06:56 +0900

    feat: border width 추가할 수 있게 추가 (#11)
    * feat: border width 추가할 수 있게 추가
    
    * chore: 0.6.5
    
    * refactor: 줄바꿈 수정

--- src/Event/Event.js
+++ src/Event/Event.js
@@ -99,8 +99,8 @@ const Event = ({
           width: currentWidth,
           backgroundColor: event.color,
           transform: translatedByDrag.getTranslateTransform(),
-          borderWidth: event.color === '#ffffff' ? 1 : 0,
-          borderRadius: event.color === '#ffffff' ? 4 : 0,
+          borderWidth: event.borderWidth,
+          borderRadius: 8,
           borderColor: event.borderColor
         },
         containerStyle,
