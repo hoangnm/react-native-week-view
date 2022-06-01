a9289a7d2e98357139ae920a646fd6fbffe26f1e
Pablo Pino <pdpino@uc.cl>
2022-06-01 17:16:05 +0100

    Fix linter in utils-gestures.js

--- src/utils-gestures.js
+++ src/utils-gestures.js
@@ -1,10 +1,10 @@
 /**
  * Patch a react-native-gesture-handler (RNGH) issue.
  *
- * Goal: replacing <TouchableWithoutFeedback/> with the version from RNGH, so it behaves
- *   correctly with the other gestures (event dragging, pressing, etc)
+ * Goal: using a <TouchableWithoutFeedback/> that behaves correctly
+ *   with the other gestures from RNGH (dragging, pressing, etc).
  *
- * RNGH provides these replacements, e.g.:
+ * RNGH provides replacements, e.g.:
  *   import { TouchableWithoutFeedback } from 'react-native-gesture-handler'
  *   (see here:
  *   https://docs.swmansion.com/react-native-gesture-handler/docs/api/components/touchables)
@@ -16,23 +16,14 @@
  * This utils-gestures.js file provides the <ViewWithTouchable/> component:
  *   a simple View plus onPress and onLongPress callbacks for receiving touches.
  *   The view provides no visual feedback, but uses gestures of RNGH, so it fits our goal.
- *
- * The hoc withTouchableGestures() could also be used to add the onPress and onLongPress callback to
- *   other components (though I do not see the point right now).
- *
- * Note: I'm not sure if this is too different from createNativeWrapper():
- *   https://docs.swmansion.com/react-native-gesture-handler/docs/gesture-handlers/api/create-native-wrapper
- *   That hoc works with the old RNGH API that uses
- *   components (e.g. <TapGestureHandler />, etc)
  */
 import React from 'react';
 import { View } from 'react-native';
 import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 import { runOnJS } from 'react-native-reanimated';
 
-
 const withTouchableGestures = (Component) => {
-  const _viewWithTouchable = ({ onPress, onLongPress, ...other }) => {
+  const viewWithTouchable = ({ onPress, onLongPress, ...other }) => {
     const pressGesture = Gesture.Tap()
       .enabled(!!onPress)
       .onEnd((evt, success) => {
@@ -52,11 +43,13 @@ const withTouchableGestures = (Component) => {
     const composedGesture = Gesture.Exclusive(longPressGesture, pressGesture);
     return (
       <GestureDetector gesture={composedGesture}>
-        <Component {...other}/>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <Component {...other} />
       </GestureDetector>
-    )
+    );
   };
-  return _viewWithTouchable;
+  return viewWithTouchable;
 };
 
+// eslint-disable-next-line import/prefer-default-export
 export const ViewWithTouchable = withTouchableGestures(View);
