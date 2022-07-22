20716e6315402e72d5dd7a24f1c8993edfdfdc64
Pablo Pino <pdpinog@gmail.com>
2022-07-22 12:23:23 +0100

    [stack-vs-lane] Simplify event position calculation

--- src/Events/position.js
+++ src/Events/position.js
@@ -1,42 +1,39 @@
 import { minutesInDay } from '../utils/dates';
 import { minutesInDayToTop, minutesToHeight } from '../utils/dimensions';
 
-const EVENT_HORIZONTAL_PADDING = 8; // percentage
-const STACK_OFFSET = 15; // percentage
+const EVENT_HORIZONTAL_PADDING = 8 / 100;
+const STACK_OFFSET_FRACTION = 15 / 100;
 const MIN_ITEM_WIDTH = 4; // pixels
-const MIN_ITEM_PADDING = 2;
-
-const padItemWidth = (width, paddingPercentage = EVENT_HORIZONTAL_PADDING) =>
-  paddingPercentage > 0
-    ? width - Math.max(MIN_ITEM_PADDING, (width * paddingPercentage) / 100)
-    : width;
 
 const computeWidthByLane = (overlap, dayWidth) =>
   dayWidth / (overlap.nLanes || 1);
 
-const computePaddedWidth = (overlap, dayWidth) => {
+const computeHorizontalPadding = (overlap, dayWidth) => {
   const widthByLane = computeWidthByLane(overlap, dayWidth);
   const paddingByLane = EVENT_HORIZONTAL_PADDING / (overlap.nLanes || 1);
-  return padItemWidth(widthByLane, paddingByLane);
+  return widthByLane * paddingByLane;
 };
 
 const computeStackOffset = (overlap, dayWidth) => {
   const widthByLane = computeWidthByLane(overlap, dayWidth);
-  const stackOffset = (STACK_OFFSET * widthByLane) / 100;
-  return stackOffset * (overlap.stackPosition || 0);
+  return widthByLane * STACK_OFFSET_FRACTION * (overlap.stackPosition || 0);
 };
 
 export const computeWidth = (overlap, dayWidth) => {
   const width =
-    computePaddedWidth(overlap, dayWidth) -
+    computeWidthByLane(overlap, dayWidth) -
+    computeHorizontalPadding(overlap, dayWidth) -
     computeStackOffset(overlap, dayWidth);
   return Math.max(width, MIN_ITEM_WIDTH);
 };
 
+const computeLaneOffset = (overlap, dayWidth) =>
+  computeWidthByLane(overlap, dayWidth) * (overlap.lane || 0);
+
 export const computeLeft = (overlap, dayWidth) => {
-  const widthByLane = computeWidthByLane(overlap, dayWidth);
-  const stackOffset = computeStackOffset(overlap, dayWidth);
-  const left = widthByLane * (overlap.lane || 0) + stackOffset;
+  const left =
+    computeLaneOffset(overlap, dayWidth) +
+    computeStackOffset(overlap, dayWidth);
   return Math.min(left, dayWidth);
 };
 
