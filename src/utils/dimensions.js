1a6f52da55b414da14a6fee428ea084d0095f24a
Pablo Pino <pdpinog@gmail.com>
2022-08-25 08:42:18 -0400

    Fix timesColumnWidth percentage parsing

--- src/utils/dimensions.js
+++ src/utils/dimensions.js
@@ -95,17 +95,23 @@ export const topToSecondsInDay = (
 const DEFAULT_TIMES_WIDTH_PERCENTAGE = 18;
 
 /**
- * A number in range 0..1 is parsed as a percentage,
- * otherwise as the value itself (e.g. pixels).
+ * Get the rawPageWidth configured by the timesColumnWidth prop.
  *
- * @param {Number} value
+ * If timesColumnWidth is in range 0..1 is used as a fraction of the
+ * total-width, otherwise as the amount of points in the screen.
+ *
+ * @param {Number} weekViewWidth
+ * @param {Number} timesColumnWidth
  * @returns Number
  */
-const resolveNumberAsPercentage = (value) => {
-  if (value > 0 && value < 1) {
-    return value * 100;
+const computeRawPageWidth = (weekViewWidth, timesColumnWidth) => {
+  if (timesColumnWidth > 0 && timesColumnWidth < 1) {
+    return weekViewWidth * (1 - timesColumnWidth);
   }
-  return value;
+  if (timesColumnWidth > 0 && timesColumnWidth < weekViewWidth) {
+    return weekViewWidth - timesColumnWidth;
+  }
+  return (weekViewWidth * (100 - DEFAULT_TIMES_WIDTH_PERCENTAGE)) / 100;
 };
 
 export const computeHorizontalDimensions = (
@@ -113,20 +119,17 @@ export const computeHorizontalDimensions = (
   numberOfDays,
   timesColumnWidth = DEFAULT_TIMES_WIDTH_PERCENTAGE,
 ) => {
-  const pageWidthPercentage =
-    (100 - resolveNumberAsPercentage(timesColumnWidth)) / 100;
+  const rawPageWidth = computeRawPageWidth(totalWidth, timesColumnWidth);
 
-  // Each day must have an equal width (integer pixels)
-  const dayWidth = Math.floor(
-    (totalWidth * pageWidthPercentage) / numberOfDays,
-  );
-  const pageWidth = numberOfDays * dayWidth;
+  // Each day must have an equal width (integer points)
+  const dayWidth = Math.floor(rawPageWidth / numberOfDays);
+  const exactPageWidth = numberOfDays * dayWidth;
 
   // Fill the full screen
-  const timeLabelsWidth = totalWidth - pageWidth;
+  const timeLabelsWidth = totalWidth - exactPageWidth;
 
   return {
-    pageWidth,
+    pageWidth: exactPageWidth,
     timeLabelsWidth,
     dayWidth,
   };
