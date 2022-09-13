fcba1cc1ed526de8b37222007e29b3f39f03c8fd
Kirill Efimov <korifey91@gmail.com>
2022-09-13 10:36:12 +0300

    Feat: update types after updating to 0.22 version

--- index.d.ts
+++ index.d.ts
@@ -32,55 +32,7 @@ export interface WeekViewProps {
   selectedDate: Date;
   numberOfDays: 1 | 3 | 5 | 7;
 
-  formatDateHeader?: string;
-  formatTimeLabel?: string;
-  /**
-   * Show or hide the selected month and year in the top-left corner (a.k.a the title).
-   */
-  showTitle?: boolean;
-  showNowLine?: boolean;
-  nowLineColor?: string;
-  fixedHorizontally?: boolean;
-  isRefreshing?: boolean;
-  hoursInDisplay?: number;
-  beginAgendaAt?: number;
-  endAgendaAt?: number;
-  timeStep?: number;
-  locale?: string;
-  rightToLeft?: boolean;
-
-  prependMostRecent?: boolean;
-
-  headerStyle?: StyleProp<ViewStyle>;
-  headerTextStyle?: StyleProp<ViewStyle>;
-  hourTextStyle?: StyleProp<TextStyle>;
-  eventContainerStyle?: StyleProp<ViewStyle>;
-  gridRowStyle?: StyleProp<ViewStyle>;
-  gridColumnStyle?: StyleProp<ViewStyle>;
-
-  EventComponent?: React.FC<EventComponentProps>;
-  TodayHeaderComponent?: React.FC<HeaderComponentProps>;
-  DayHeaderComponent?: React.FC<HeaderComponentProps>;
-  footerComponent?: React.FC<HeaderComponentProps>;
-  RefreshComponent?: React.Component;
-
-  // Week-view customizations
-  /**
-   * Vertical position of the week-view in the first render (vertically in the agenda).
-   *
-   * Default value: 8 (8 am)
-   */
-  startHour?: number
-
-  /**
-   * First day of the week, i.e. day to show at the left of the week-view
-   * (0 is Sunday, 1 is Monday, and so on). Only useful when numberOfDays === 7
-   * or fixedHorizontally is true.
-   *
-   * Default value: 1 (Monday)
-   */
-  weekStartsOn?: number;
-
+  // Gesture interactions
   /**
    * Callback when an event item is pressed, receives the event-item pressed
    * @param event
@@ -132,18 +84,6 @@ export interface WeekViewProps {
   onGridLongPress?(pressEvent: TouchEvent, startHour: number, date: Date): void;
 
   /**
-   * Callback when an event item is dragged to another position.
-   * Arguments: event: event-item moved,and the newStartDate and newEndDate
-   * are Date objects with day and hour of the new position (precision up
-   * to minutes). With this callback you must trigger an update on the events
-   * prop (i.e. update your DB), with the updated information from the event.
-   * @param event
-   * @param newStartDate
-   * @param newEndDate
-   */
-  onDragEvent?(event: Event, newStartDate: Date, newEndDate: Date): void;
-
-  /**
    * Callback when a day from the header is pressed.
    * @param date
    * @param formattedDate
@@ -162,6 +102,103 @@ export interface WeekViewProps {
    * @param dateWithTime
    */
   onTimeScrolled?(dateWithTime: Date): void;
+
+  /**
+   * Callback when an event item is dragged to another position.
+   * Arguments: event: event-item moved,and the newStartDate and newEndDate
+   * are Date objects with day and hour of the new position (precision up
+   * to minutes). With this callback you must trigger an update on the events
+   * prop (i.e. update your DB), with the updated information from the event.
+   * @param event
+   * @param newStartDate
+   * @param newEndDate
+   */
+  onDragEvent?(event: Event, newStartDate: Date, newEndDate: Date): void;
+
+  /**
+   * Callback when an event item is edited by dragging its borders.
+   * @param event
+   * @param newStartDate
+   * @param newEndDate
+   */
+  onEditEvent?(event: Event, newStartDate: Date, newEndDate: Date): void;
+
+  editEventConfig?: { bottom: boolean; top: boolean; left: boolean; right: boolean; };
+
+  // Week-view customizations
+  /**
+   * Vertical position of the week-view in the first render (vertically in the agenda).
+   *
+   * Default value: 8 (8 am)
+   */
+  startHour?: number
+
+  /**
+   * First day of the week, i.e. day to show at the left of the week-view
+   * (0 is Sunday, 1 is Monday, and so on). Only useful when numberOfDays === 7
+   * or fixedHorizontally is true.
+   *
+   * Default value: 1 (Monday)
+   */
+  weekStartsOn?: number;
+
+  /**
+   * Show or hide the selected month and year in the top-left corner (a.k.a the title).
+   */
+  showTitle?: boolean;
+
+  hoursInDisplay?: number;
+
+  beginAgendaAt?: number;
+  endAgendaAt?: number;
+  timeStep?: number;
+  formatDateHeader?: string;
+  formatTimeLabel?: string;
+  timesColumnWidth?: number;
+
+  EventComponent?: React.FC<EventComponentProps>;
+  TodayHeaderComponent?: React.FC<HeaderComponentProps>;
+  DayHeaderComponent?: React.FC<HeaderComponentProps>;
+
+  showNowLine?: boolean;
+  nowLineColor?: string;
+  fixedHorizontally?: boolean;
+  isRefreshing?: boolean;
+
+  RefreshComponent?: React.Component;
+
+  locale?: string;
+  rightToLeft?: boolean;
+
+  // Style props
+  headerStyle?: StyleProp<ViewStyle>;
+  headerTextStyle?: StyleProp<ViewStyle>;
+  hourTextStyle?: StyleProp<TextStyle>;
+  eventContainerStyle?: StyleProp<ViewStyle>;
+
+  // Grid lines props
+  gridRowStyle?: StyleProp<ViewStyle>;
+  gridColumnStyle?: StyleProp<ViewStyle>;
+
+  // Horizontal list optimizations
+  windowSize?: number;
+  initialNumToRender?: number;
+  maxToRenderPerBatch?: number;
+  updateCellsBatchingPeriod?: number;
+
+
+  // Other props (patch RN bugs)
+  prependMostRecent?: boolean;
+
+
+
+
+
+
+
+
+
+
 }
 
 declare const WeekView: React.ComponentType<WeekViewProps>;
