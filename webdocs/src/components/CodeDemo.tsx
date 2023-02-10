5930a1f5d7d7c475dd4eda72f315e39d94b66139
Pablo Pino <pdpinog@gmail.com>
2023-02-10 20:39:26 -0300

    Create CodeDemo component
    Use it in BlockEvents

---
+++ webdocs/src/components/CodeDemo.tsx
@@ -0,0 +1,22 @@
+// eslint-disable-next-line no-use-before-define
+import React from 'react';
+import styles from './styles.module.css';
+
+type Props = {
+  imgSrc: string;
+  alt?: string;
+  children: JSX.Element;
+};
+
+const CodeDemo: React.FC<Props> = ({ imgSrc, children, alt = 'image' }) => {
+  return (
+    <div className={styles.container}>
+      <div>
+        <img className={styles.demo} src={imgSrc} alt={alt} />
+      </div>
+      <div>{children}</div>
+    </div>
+  );
+};
+
+export default CodeDemo;
